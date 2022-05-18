67b377a95912ab4161e43c70823c9044d255e691
prashantfs <85509055+prashantfs@users.noreply.github.com>
2022-05-18 19:14:30 +0530

    Initial commit

---
+++ src/app/shared/article-helpers/article-preview.component.ts
@@ -0,0 +1,27 @@
+import { Component, Input } from '@angular/core';
+
+import { Article } from '../../core';
+
+@Component({
+  selector: 'app-article-preview',
+  templateUrl: './article-preview.component.html'
+})
+export class ArticlePreviewComponent {
+  @Input() article: any;
+
+
+  ngOnInit() {
+
+  console.log(this.article)
+  }
+
+  onToggleFavorite(favorited: boolean) {
+    this.article['favorited'] = favorited;
+
+    if (favorited) {
+      this.article['favoritesCount']++;
+    } else {
+      this.article['favoritesCount']--;
+    }
+  }
+}
---
+++ src/environments/environment.prod.ts
@@ -0,0 +1,4 @@
+export const environment = {
+  production: true,
+  api_url: 'https://dev.to/api/articles?per_page=100&tags='
+};
