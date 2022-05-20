f3e5287bd707d0cbae35a4ee1d1695c832a4f5b6
prashantfs <85509055+prashantfs@users.noreply.github.com>
2022-05-20 10:09:34 +0530

    build

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,4 +1,4 @@
 export const environment = {
   production: true,
-  api_url: 'https://1c12-2409-4066-e97-7c99-1dff-ed95-3892-8297.ngrok.io'
+  api_url: 'https://1c12-2409-4066-e97-7c99-1dff-ed95-3892-8297.ngrok.io/'
 };
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -5,5 +5,5 @@
 
 export const environment = {
   production: false,
-  api_url: 'https://1c12-2409-4066-e97-7c99-1dff-ed95-3892-8297.ngrok.io'
+  api_url: 'https://1c12-2409-4066-e97-7c99-1dff-ed95-3892-8297.ngrok.io/'
 };
